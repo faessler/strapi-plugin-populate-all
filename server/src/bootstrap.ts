48835fece568d3e1e75c7d7d1ce328c2a4d893cb
Jan Fässler <git@faessler.be>
2025-06-04 09:19:06 +0200

    fix(bootstrap): make bootstrap fail safe

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -3,19 +3,23 @@ import { getPopulateQuery } from './utils/getPopulateQuery';
 
 const bootstrap = ({ strapi }: { strapi: Core.Strapi }) => {
   strapi.db.lifecycles.subscribe((event) => {
-    // @ts-expect-error `Property 'recursive' does not exist on type 'Params'.ts(2339)`
-    if (event.params?.recursive === 'true') {
-      if (event.action === 'beforeFindMany' || event.action === 'beforeFindOne') {
-        strapi.log.debug(`[populate-all] recursively populate ${event.model.uid}`);
+    try {
+      // @ts-expect-error `Property 'recursive' does not exist on type 'Params'.ts(2339)`
+      if (event.params?.recursive === 'true') {
+        if (event.action === 'beforeFindMany' || event.action === 'beforeFindOne') {
+          strapi.log.debug(`[populate-all] recursively populate ${event.model.uid}`);
 
-        const populateQuery = getPopulateQuery(event.model.uid as UID.Schema);
-        if (populateQuery?.populate) {
-          strapi.log.debug(
-            `[populate-all] populate query for ${event.model.uid}: ${JSON.stringify(populateQuery.populate)}`
-          );
-          event.params.populate = populateQuery.populate;
+          const populateQuery = getPopulateQuery(event.model.uid as UID.Schema);
+          if (populateQuery?.populate) {
+            strapi.log.debug(
+              `[populate-all] populate query for ${event.model.uid}: ${JSON.stringify(populateQuery.populate)}`
+            );
+            event.params.populate = populateQuery.populate;
+          }
         }
       }
+    } catch (error) {
+      strapi.log.error(`[populate-all] failed to apply populate db query: ${error}`);
     }
   });
 };
