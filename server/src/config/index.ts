0c5f33990b634a44f5fe538a29a9f1bb39a685a6
Jan Fässler <git@faessler.be>
2025-06-04 13:32:28 +0200

    revert(config): undo validator because it caused some problems

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,27 +2,16 @@ export default {
   default: {
     relations: true,
   },
-  validator(config: Record<string, unknown>) {
-    for (const [key, value] of Object.entries(config)) {
-      switch (key) {
-        case 'relations': {
-          const isBoolean = typeof value === 'boolean';
-          const isArrayOfStrings =
-            Array.isArray(value) &&
-            value?.every((relation: unknown) => typeof relation === 'string');
-          if (!(isBoolean || isArrayOfStrings)) {
-            throw new Error(
-              `[populate-all] config "${key}" of type ${typeof value} is not valid. Supported are boolean or Array of strings.`
-            );
-          }
-          break;
-        }
+  validator(config) {
+    const isBoolean = typeof config?.relations === 'boolean';
+    const isArrayOfStrings =
+      Array.isArray(config?.relations) &&
+      config?.relations?.every((relation: unknown) => typeof relation === 'string');
 
-        default:
-          strapi.log.warn(
-            `[populate-all] unknown config "${key}" provided. This config will be ignored.`
-          );
-      }
+    if (!(isBoolean || isArrayOfStrings)) {
+      throw new Error(
+        `[populate-all] config "relations" of type ${typeof config?.relation} is not valid. Supported are boolean or Array of strings.`
+      );
     }
   },
 };
