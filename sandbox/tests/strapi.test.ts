fd4f72319b93a3159c1c9ff5341f64a173eba628
Jan Fässler <git@faessler.be>
2025-07-04 08:56:18 +0200

    ci(test): add tests to ci

---
+++ sandbox/tests/strapi.test.ts
@@ -0,0 +1,14 @@
+import { strapiRequest } from './strapi';
+import { describe, expect, test } from '@jest/globals';
+
+describe('strapi', () => {
+  test('strapi is started', async () => {
+    const response = await strapiRequest.get('/');
+    expect(response.statusCode).toBe(302);
+  });
+
+  test('data is seeded', async () => {
+    const response = await strapiRequest.get('/api/articles');
+    expect(response.statusCode).toBe(200);
+  });
+});
