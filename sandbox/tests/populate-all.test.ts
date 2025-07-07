921201f04045024d9dde85387bfccb851091d8f2
Jan Fässler <faessler@users.noreply.github.com>
2025-07-07 22:05:54 +0200

    fix(loop): add check to prevent infinite loops (#8)
    * fix(loop): add check to prevent infinite loops
    
    * ci(test): build plugin for testing

--- sandbox/tests/populate-all.test.ts
+++ sandbox/tests/populate-all.test.ts
@@ -2,10 +2,23 @@ import { describe, expect, test } from "@jest/globals";
 import { strapiRequest } from "./strapi";
 
 describe("strapi-plugin-populate-all", () => {
-  test.skip("if everything is populated", async () => {
+  test("if everything is populated", async () => {
     const response = await strapiRequest.get(
       "/api/articles?status=draft&populate=all"
     );
+
+    // request succeeds
     expect(response.statusCode).toBe(200);
+
+    // has first level of population
+    expect(response.body.data[0]).toHaveProperty("cover");
+    expect(response.body.data[0]).toHaveProperty("author");
+    expect(response.body.data[0]).toHaveProperty("category");
+    expect(response.body.data[0]).toHaveProperty("blocks");
+
+    // doesn't loop
+    expect(response.body.data[0].category.articles[0]).not.toHaveProperty(
+      "category"
+    );
   });
 });
