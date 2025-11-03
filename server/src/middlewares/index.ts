bf4e3d4193bec181535447c907fd12d5a5458929
Jan Fässler <git@faessler.be>
2025-11-03 10:24:58 +0100

    fix(docs): update docs for custom recursive populate key

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -2,8 +2,8 @@ export default {
   /**
    * This is a global middleware to add support for the custom query param `?populate=all`.
    * Since Strapi's validator does not allow custom values for the populate param, we intercept the request here.
-   * If `?populate=all` is detected, we omit the value and set `?recursive=true` instead.
-   * The bootstrap script later picks up `?recursive=true` to apply the desired populate logic.
+   * If `?populate=all` is detected, we omit the value and set `?populateAll=true` instead.
+   * The bootstrap script later picks up `?populateAll=true` to apply the desired populate logic.
    */
   populateAll: async (ctx, next) => {
     if (ctx.query.populate === "all") {
